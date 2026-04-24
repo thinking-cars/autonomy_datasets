c13e84b78021aa9362679d0c1fb0fdea3b0092c6
Raphael van Kempen <vankempen@thinking-cars.de>
2026-04-24 11:30:56 +0000

    add zenoh router config

--- autonomy_datasets/launch/autonomy_datasets.launch.py
+++ autonomy_datasets/launch/autonomy_datasets.launch.py
@@ -20,7 +20,7 @@ def generate_launch_description():
     args = [
         DeclareLaunchArgument(
             "dataset",
-            default_value="nvidia_physicalai_av_dataset",
+            default_value="nuscenes",
             description="dataset name",
             choices=["nvidia_physicalai_av_dataset", "waymo_open_dataset", "nuscenes"],
         ),

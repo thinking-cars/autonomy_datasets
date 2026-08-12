2aaeae614f1a238562fa158bcb0bf51fc3c5e790
Lukas Zanger <lukas.zanger@ika.rwth-aachen.de>
2026-08-12 14:33:47 +0000

    update Copyright notice

--- autonomy_datasets/autonomy_datasets/datasets/truckscenes/__init__.py
+++ autonomy_datasets/autonomy_datasets/datasets/truckscenes/__init__.py
@@ -1,4 +1,4 @@
-# Copyright Thinking Cars GmbH
+# Copyright Institute for Automotive Engineering (ika), RWTH Aachen University
 # SPDX-License-Identifier: Apache-2.0
 
 """Adapter for the MAN TruckScenes dataset."""

8f22c2e5caee7748f2082e31cb2be8de5f0a2049
Raphael van Kempen <vankempen@thinking-cars.de>
2026-09-07 13:01:26 +0000

    fix pre-commit checks

--- autonomy_datasets/autonomy_datasets/datasets/rosbag/__init__.py
+++ autonomy_datasets/autonomy_datasets/datasets/rosbag/__init__.py
@@ -1,2 +1,4 @@
 # Copyright Thinking Cars GmbH
 # SPDX-License-Identifier: Apache-2.0
+
+"""ROS bag dataset support."""
--- autonomy_datasets/autonomy_datasets/datasets/waymo_open_dataset/__init__.py
+++ autonomy_datasets/autonomy_datasets/datasets/waymo_open_dataset/__init__.py
@@ -1,2 +1,4 @@
 # Copyright Thinking Cars GmbH
 # SPDX-License-Identifier: Apache-2.0
+
+"""Waymo Open Dataset integration package."""
